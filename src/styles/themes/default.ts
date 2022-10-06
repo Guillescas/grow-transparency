5b481568b7875adcb2de4052b3dfe995aad82546
Guilherme Illescas <guilhermeillescas@Guilhermes-MacBook-Pro-2.local>
2022-10-06 20:31:13 -0300

    feat: added styled-components

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,13 @@
+export const theme = {
+  colors: {
+    black: '#000000',
+    white: '#fff'
+  },
+
+  font: {
+    family: {
+      DM: 'DM Sans, sans-serif',
+      Mono: 'Roboto Mono'
+    }
+  }
+}
