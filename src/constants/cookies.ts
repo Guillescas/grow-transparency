221e169375dec7214f6906766c9d2bbe7bff1ae5
Guilherme Illescas <guilhermeillescas@Guilhermes-MacBook-Pro-2.local>
2022-10-13 19:07:10 -0300

    feat: added cookies constants

---
+++ src/constants/cookies.ts
@@ -0,0 +1,5 @@
+export const cookiesNames = {
+  user: '@grow-transparency:user-1.0.0',
+  token: '@grow-transparency:access-token-1.0.0',
+  refreshToken: '@grow-transparency:refresh-token-1.0.0'
+}
